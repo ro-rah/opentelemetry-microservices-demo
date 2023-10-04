6d47a2b09cc5b6b0d0ace5a876555f8eb7eb9d6d
Ro Nak <ronakulus@nonyabiz.com>
2023-10-04 00:09:51 -0500

    install go

--- shipping.sh
+++ shipping.sh
@@ -13,6 +13,11 @@ otelCollectorName=otelcollector
 otelCollector="http://$otelCollectorName:4317"
 
 cd src/shippingservice
+#install go
+wget -nv -O go.tar.gz https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
 # Get Sealights Agent
 echo "[Sealights] Downloading Sealights Golang & CLI Agents..."
 wget -nv -O sealights-go-agent.tar.gz https://agents.sealights.co/slgoagent/latest/slgoagent-linux-amd64.tar.gz
