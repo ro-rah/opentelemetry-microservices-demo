91f7c2d1154a3a78c14e006a7f0531e99b0e0222
Ro Nak <ronakulus@nonyabiz.com>
2023-10-04 00:11:14 -0500

    perms

--- shipping.sh
+++ shipping.sh
@@ -15,7 +15,8 @@ otelCollector="http://$otelCollectorName:4317"
 cd src/shippingservice
 #install go
 wget -nv -O go.tar.gz https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go.tar.gz
+sudo rm -rf /usr/local/go 
+sudo tar -C /usr/local -xzf go.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # Get Sealights Agent
